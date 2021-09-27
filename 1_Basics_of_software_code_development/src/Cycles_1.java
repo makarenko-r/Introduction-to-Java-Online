41befa7b242175ea067f732af6117f0f9ebfe853
makarenko-r <java.makarenko@gmail.com>
2021-09-28 02:10:50 +0300

    Create Cycles_1.java

---
+++ 1_Basics_of_software_code_development/src/Cycles_1.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Cycles_1 {
+
+    public static void main(String[] args) {
+        int x, output;
+        System.out.println("Циклы. Задача №1.");
+        // Ввод значения x
+        while (true) {
+            try {
+                System.out.print("Введите любое целое положительное число, x = ");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                String input = reader.readLine();
+                x = Integer.parseInt(input);
+                if (x>0) break;                 // Проверка на 0 и отрицательные
+                else throw new Exception();     // Выброс исключения если 0 или -
+            } catch (Exception ex) {
+                System.out.println("Вы ввели неверное значение. Повторите ввод.");
+            }
+        }
+        // Вычисление значения суммы от 0 до x
+        output = 0;
+        for (int i=1; i<=x; i++) {
+            output+=i;
+        }
+        // Вывод результата
+        System.out.println("Сумма от 1 до " + x + " равна " + output + ".");
+    }
+}
