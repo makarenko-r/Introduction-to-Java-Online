8ac92b37ea0614bf3d7fc5a4c5da29a6ccf586f8
makarenko-r <java.makarenko@gmail.com>
2021-09-27 15:26:05 +0300

    Create Branching_5.java

---
+++ 1_Basics_of_software_code_development/src/Branching_5.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Branching_5 {
+
+    public static void main(String[] args) {
+        double x, output;
+        System.out.println("Ветвление. Задача №5.");
+        // Ввод значения x
+        while (true) {
+            try {
+                System.out.print("Введите значение, x = ");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                String input = reader.readLine();
+                x = Double.parseDouble(input);
+                break;
+            } catch (Exception ex) {
+                System.out.println("Вы ввели неподобающее значение. Повторите ввод.");
+            }
+        }
+        // Вычисление значения функции
+        if (x <= 3) output = x*x - 3*x + 9;
+        else output = 1 / (x*x*x + 6);
+        System.out.println("Значение функции равно:\nF(x) =  " + output);
+    }
+}
