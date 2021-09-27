8dd038c3c6b7e6a59417ddb4d5f3967682bda63b
makarenko-r <java.makarenko@gmail.com>
2021-09-27 14:35:53 +0300

    Create Linear_5.java

---
+++ 1_Basics_of_software_code_development/src/Linear_5.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Linear_5 {
+
+    public static void main(String[] args) {
+        int T;
+        String output;
+        System.out.println("Линейные программы. Задача №5.");
+        // Ввод значения T
+        while (true) {
+            try {
+                System.out.print("Введите натуральное число, Т = ");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                String input = reader.readLine();
+                T = Integer.parseInt(input);
+                if (T < 0) throw new Exception();
+                break;
+            } catch (Exception ex) {
+                System.out.println("Вы ввели неверное значение. Повторите ввод.");
+            }
+        }
+
+        // Вычисление значения выражения
+        output = T/3600 + "ч " + T % 3600 / 60 + "мин " + T % 60 + "с";
+        // Вывод результата
+        System.out.println("Значение длительности времени равно " + output + ".");
+    }
+}
