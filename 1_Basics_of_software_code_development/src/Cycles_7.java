907e848fd24001022dda8d72389e9f12f1e60ef3
makarenko-r <java.makarenko@gmail.com>
2021-09-29 23:39:48 +0300

    Create Cycles_7.java

---
+++ 1_Basics_of_software_code_development/src/Cycles_7.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Cycles_7 {
+
+    public static void main(String[] args) {
+        int m, n;
+        System.out.println("Циклы. Задача №7.");
+        // Ввод значения m
+        while (true) {
+            try {
+                System.out.print("Введите натуральное число m = ");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                String input = reader.readLine();
+                m = Integer.parseInt(input);
+                if (m <= 0) throw new Exception();      // натуральное число больше 0
+                break;
+            } catch (Exception ex) {
+                System.out.println("Вы ввели неподобающее значение. Повторите ввод.");
+            }
+        }
+        // Ввод значения n
+        while (true) {
+            try {
+                System.out.print("Введите натуральное число, n = ");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                String input = reader.readLine();
+                n = Integer.parseInt(input);
+                if (n <= m) throw new Exception(); // [m,n] - n - по определению не может быть меньше либо равно m
+                break;
+            } catch (Exception ex) {
+                System.out.println("Вы ввели неподобающее значение. Повторите ввод.");
+            }
+        }
+        // Вывод делителей числа кроме единицы и самого числа
+        for (int i = m; i <= n; i++) {
+            System.out.print("Делители для числа " + i + ": ");
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
